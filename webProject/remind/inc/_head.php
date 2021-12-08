5bc1fd524ec46afe13a1e28efe69de2aeaff3285
oh2279 <crazy12394@gmail.com>
2021-12-08 15:19:43 +0900

    webProject(full)

---
+++ webProject/remind/inc/_head.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../CafeCss.css">
+    <link href="./css/customs.css?after1" rel="stylesheet" >
+
+	<title>교육신청게시판</title>
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+	  <div class="container-fluid">
+		<div class="navbar-header">
+		  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>                        
+		  </button>
+		  <a class="navbar-brand" href="../index.html">Cafe_Cross</a>
+		</div>
+		<div class="collapse navbar-collapse" id="myNavbar">
+		  <ul class="nav navbar-nav" id = "liNavbar">
+			<li><a href="../index.html">홈</a></li>
+			<li><a href="../menu.html">메뉴소개</a></li>
+			<li><a href="../road.html">오시는 길</a></li>
+			<li class="active" ><a href="../remind/index.php">교육신청게시판</a></li>
+		  </ul>
+		  <ul class="nav navbar-nav navbar-right">
+           <li><a href="../login.html"><span class="glyphicon glyphicon-user"></span> 로그인</a></li>
+           <li><a href="../Signin.html"><span class="glyphicon glyphicon-log-in"></span> 회원가입</a></li>
+         </ul>
+		</div>
+	  </div>
+</nav>
+
---
+++ webProject/remind/inc/_tail.php
@@ -0,0 +1,13 @@
+<footer class="container-fluid text-center" id = "footer">
+		<div id="navigation">
+			<div class="flex-container">
+				<div><a href="https://www.instagram.com/cross20130901/?hl=ko"><img src="../images/insta.jpg" 	width="80" height="80" alt="insta" /></a></div>
+			</div>
+			
+		</div>
+		<h2>영업시간 안내</h2>
+		<p>연중무휴<br/>11:00 ~ 22:00<br/></p>
+</footer>
+
+</body>
+</html>
